ddd3bd062fa4b019ead73500ffe86b9551c82ed0
Vinicius  Prates <89698942+vinicius-prates@users.noreply.github.com>
2022-05-13 16:55:02 -0300

    Create Lanche.java

---
+++ Lanche.java
@@ -0,0 +1,12 @@
+package base;
+
+public class Lanche {
+    String nome;
+    double preco;
+
+    Lanche(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+}
