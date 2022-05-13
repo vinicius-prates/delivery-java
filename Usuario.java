46ab904de7cc956c2fee9be939baf8e61ad4c356
Vinicius  Prates <89698942+vinicius-prates@users.noreply.github.com>
2022-05-13 16:54:48 -0300

    Create Usuario.java

---
+++ Usuario.java
@@ -0,0 +1,22 @@
+package base;
+
+public class Usuario {
+    // Variaveis
+    String nome;
+    String enderco;
+    String cpf;
+    String telefone;
+
+    //Construtor
+    Usuario(String nome, String endereco, String cpf, String telefone){
+       this.nome = nome;
+       this.enderco = endereco;
+       this.cpf = cpf;
+       this.telefone = telefone;
+
+    }
+
+
+
+
+}
